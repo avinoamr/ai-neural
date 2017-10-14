dfa2a46aae37be5f113376271d23e9c0500e292e
Roi Avinoam <avinoamr@gmail.com>
2017-10-14 15:44:09 -0700

    Compute the bias as a weight

--- learn_ml/04_regression.py
+++ learn_ml/04_regression.py
@@ -15,10 +15,21 @@
 # Where the x values are given. All that's left for us to do is to discover the
 # weights (a, b, c, ...) & bias (C). These weights are encoded in a w-vector
 # that we'll need to learn as we're given examples of inputs and outputs.
-# Similarily the bias is a single scalar we'll need to learn.
 #
-# [1] Of course, that's an unsubstentiated assumption to make for many
-# use-cases, but we'll see later how to generalize for non-linear functions
+# Similarily the bias is a single scalar we'll need to learn, in exactly the
+# same way as the other weights. So supporting it might include a duplication
+# of the same code used for the weights. Instead of duplicating the code, we can
+# just regard the bias as 1 extra weight to be learned (N + 1), with the
+# exception that the input for that weight is always 1. In other words, the bias
+# behaves just like a normal weight for an input that's always fixed at 1.
+#
+# [1] Of course, that's an unsubstentiated assumption to make for many use
+# cases. Generally, there are two ways to generalize for non-linear functions:
+#   1. We can extend the input to include polynomial components. Instead of just
+#       using the input as is, we can first produce multiple copies of the same
+#       inputs, only raised to the power of 2, 3, etc.
+#   2. Using hidden layers. A concept I will maybe cover later.
+#
 import numpy as np
 
 # no constant TARGET; we need to find it.
@@ -46,8 +57,7 @@ def loss(actual, target):
 # and immutable we learn the weights by which to multiply this input. In fact,
 # these weights can represent any linear function, so all that's left to do is
 # find these weights and we have our function approximation!
-w = np.random.rand(N) * 2 - 1 # N-sized weights vector of random numbers
-b = np.random.rand() * 2 - 1 # the bias
+w = np.random.rand(1 + N) * 2 - 1 # 1-extra weight for the bias at index 0
 for j in xrange(ITERATIONS): # can we stop early once we reach our target?
 
     # first we need a new input for each iteration. In reality, we should
@@ -65,7 +75,8 @@ for j in xrange(ITERATIONS): # can we stop early once we reach our target?
     #
     # thus we need to multiply all inputs with their weights, element wise, and
     # sum up the result.
-    out = sum(inp * w, b) # adding the bias as well.
+    inp = np.insert(inp, 0, 1.) # inject a fixed input of 1 for the bias weight
+    out = sum(inp * w)
 
     # now, lets find our current loss - comparing our prediction to the actual
     # value produced by f():
@@ -75,11 +86,11 @@ for j in xrange(ITERATIONS): # can we stop early once we reach our target?
     # just as before, we now want to make infinitisimal changes to our weights,
     # in order to find how the loss changes w.r.t to every individual weight.
     # This is identical to what we did before.
-    d = np.zeros(N) # N derivatives - one per weight
-    for i in range(N):
+    d = np.zeros(1 + N) # N derivatives - one per weight
+    for i in range(1 + N):
         # add an inifinitsimal change to the current index of the input. It's an
         # immutable version of: w[i] += E
-        e = np.zeros(N)
+        e = np.zeros(1 + N)
         e[i] = E
 
         # sample the loss function after adding E to w[i]
@@ -87,24 +98,17 @@ for j in xrange(ITERATIONS): # can we stop early once we reach our target?
         # epsilon to the current weight i. Also notice that the target of our
         # loss doesn't change obviously (because the inp is the same), only the
         # predition does
-        li = loss(sum(inp * (w + e), b), target)
+        li = loss(sum(inp * (w + e)), target)
 
         # derviative of the input - or how the loss() changes w.r.t inp[i]
         d[i] = (li - l) / E
-
-    # exactly similarily, we want to compute the derivative of the loss function
-    # this time w.r.t a tiny change in the bias. You may notice that since this
-    # is an exact replication of the derivations per weight, above, we can avoid
-    # this code-duplication by regarding the bias just as one extra weight.
-    lb = loss(sum(inp * w, b + E), target)
-    db = (lb - l) / E
 
     # now we update the weights, same as before.
     # element-wise update to the new inp in the gradient direction. ie:
     #   inp[i] = STEP * d[i] * - 1 ; for every i in N = all of the inputs
     w += STEP * d * -1
-    b += STEP * db * -1 # and update the bias.
 
-# finally - lets print out our weights to see what we did:
-# you should expect the weights to resemble the ones in f().
-print "W = %s ; b = %s" % (w, b)
+# finally - lets print out our weights to see what we did. You should expect the
+# weights to resemble the ones in f(). Remember that the first weight is
+# actually the bias
+print "W = %s" % w
