96ab9387b596fe569380d94369652b380938cd28
Roi Avinoam <avinoamr@gmail.com>
2017-10-22 22:01:30 -0700

    Brevity: encode all input features in a single loop

--- learn_ml/10_titanic.py
+++ learn_ml/10_titanic.py
@@ -8,6 +8,15 @@ random.seed(1)
 data = [d for d in csv.DictReader(open("titanic.csv"))]
 N = 21
 
+vocabs = {
+    "Fare": { "cheap": 1, "low": 2, "medium": 3, "high": 4 },
+    "Embarked": { "S": 5, "C": 6, "Q": 7 },
+    "Age": { "kid": 8, "young": 9, "adult": 10, "old": 11 },
+    "Family": { "alone": 12, "small": 13, "medium": 14, "big": 15 },
+    "Pclass": { "1": 16, "2": 17, "3": 18 },
+    "Sex": { "male": 19, "female": 20 }
+}
+
 losses = []
 w = np.zeros(N)
 for i in xrange(4000):
@@ -25,29 +34,9 @@ for i in xrange(4000):
         x = np.zeros(N)
         x[0] = 1. # bias
 
-        # fare
-        idx = { "cheap": 1, "low": 2, "medium": 3, "high": 4 }[d["Fare"]]
-        x[idx] = 1.
-
-        # embarked
-        idx = { "S": 5, "C": 6, "Q": 7 }[d["Embarked"]]
-        x[idx] = 1.
-
-        # age
-        idx = { "kid": 8, "young": 9, "adult": 10, "old": 11 }[d["Age"]]
-        x[idx] = 1.
-
-        # Family Size
-        idx = { "alone": 12, "small": 13, "medium": 14, "big": 15 }[d["Family"]]
-        x[idx] = 1.
-
-        # Pclass
-        idx = { "1": 16, "2": 17, "3": 18 }[d["Pclass"]]
-        x[idx] = 1.
-
-        # sex
-        idx = { "male": 19, "female": 20 }[d["Sex"]]
-        x[idx] = 1.
+        for k, v in vocabs.items():
+            idx = v[d[k]]
+            x[idx] = 1.
 
         # encode the output
         t = float(d["Survived"])
