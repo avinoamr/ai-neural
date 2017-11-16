c0c0160ee85f3dfdd7bec268af95237d816473dd
Roi Avinoam <avinoamr@gmail.com>
2017-11-15 17:30:57 -0800

    OOP

--- 01_intro/01_gradient_descent.py
+++ 01_intro/01_gradient_descent.py
@@ -5,37 +5,73 @@
 # are numbers that indicates how the function f changes in response to a tiny
 # change in x, y. Then, we walk against that slope in the direction of steepest
 # descent - we add some amount to our initial inputs such that the f function
-# is expected to be lower. Repeat for a 1000 iterations and stop. The final
+# is expected to be lower. Repeat for a few iterations and stop. The final
 # inputs will be the local minima of the function, if one exists.
-#
-# f(x,y) - the function we want to minimize. It can be anything you want! Note
-# however, that if this function doesn't have a minima, it can get inifinitely
-# negative, and thus no minima will be found even after infinite iterations. So,
-# for this demonstration, we'll remove negatives by squaring it.
-def f(x, y):
-    return (x - y) ** 2
+import random
+random.seed(1) # static seed to make results reproducible
 
-# constants - these can be learned as well! Maybe we'll cover it later.
-E = 0.0001 # epsilon; infinitisimal size of probes to find derivatives
-STEP = 0.01 # size of the steps to take in the gradient direction
-ITERATIONS = 1000 # number of probes/step to take
+def main():
+    # f(x,y) - the function we want to minimize. It can be anything you want!
+    # Note however, that if this function doesn't have a minima, it can get
+    # inifinitely negative, and thus no minima will be found even after infinite
+    # iterations. So, for this demonstration, we'll remove negatives by squaring
+    # it.
+    f = lambda x, y: (x - y) ** 2
 
-# initial values
-x = +2. # numpy.random.rand() * 2 - 1
-y = -3. # numpy.random.rand() * 2 - 1
+    # minimize over a few iterations, printing the newly found x, y values and
+    # the output of f(x, y) for these inputs. We're expecting the code to reach
+    # an output of zero (minima of f)
+    gd = GradientDescent(f)
+    for i in xrange(150):
+        x, y, out = gd.minimize()
+        print "f(%f, %f) = %f" % (x, y, out)
 
-for i in xrange(ITERATIONS):
-    out = f(x, y)
-    print "f(%f, %f) = %f" % (x, y, out)
+# GradientDescent algorithm fixed to functions for arity 2. Given an input
+# function f, this object will gradually find inputs such that the output of the
+# function is minimized.
+class GradientDescent(object):
+    # constants/hyperparameters - these can be learned as well! Maybe we'll
+    # cover it later.
+    f = None # the function we want to minimize
+    E = 0.0001 # epsilon; infinitisimal size of probes to find derivatives
+    STEP = 0.01 # size of the steps to take in the gradient direction
 
-    # two samples, inifinitsimal points around x and y
-    outx = f(x + E, y)
-    outy = f(x, y + E)
+    # parameters (actual inputs) to be learned
+    x = 0
+    y = 0
 
-    # derivatives of x and y - or how the output of f(x, y) changes w.r.t x, y
-    dx = (outx - out) / E
-    dy = (outy - out) / E
+    # initialize for a function f. Must be of arity 2.
+    def __init__(self, f):
+        self.f = f
 
-    # update to the new x, y in the gradient direction
-    x += STEP * dx * -1 # -1 because we're after the minima, we want to walk
-    y += STEP * dy * -1 # downwards against the slope
+        # init parameters x, y with random points in the range [-1, 1]
+        self.x = random.random() * 2 - 1
+        self.y = random.random() * 2 - 1
+
+    # one-pass minimalization. Returns the new (x, y) inputs and the output
+    # computed for these inputs. It's guaranteed that if the function has a
+    # minima, it will be eventually found after enough calls to minimize()
+    def minimize(self):
+        x, y, f = self.x, self.y, self.f
+        out = f(x, y)
+
+        # two samples, inifinitsimal points around the current x and y
+        outx = f(x + self.E, y)
+        outy = f(x, y + self.E)
+
+        # derivatives of x and y - or how the output of f(x, y) changes
+        # w.r.t x, y
+        dx = (outx - out) / self.E
+        dy = (outy - out) / self.E
+
+        # update to the new x, y in the negative gradient direction
+        x += self.STEP * dx * -1 # -1 because we're after the minima, we
+        y += self.STEP * dy * -1 # want to walk downwards against the slope
+
+        self.x, self.y = x, y
+        return x, y, f(x, y)
+
+
+
+if __name__ == "__main__":
+    main()
