824ca5aedd0b2f7c7fcdd8579a03fb21ff4ca4aa
Roi Avinoam <avinoamr@gmail.com>
2017-11-24 16:03:35 -0800

    Add intuition

--- 01_intro/12_backprop.py
+++ 01_intro/12_backprop.py
@@ -46,15 +46,27 @@ class Layer(object):
     # backward pass - compute the derivatives of each weight in this layer and
     # update them.
     #
-    # This function is the heart of the back-propagation algorithm.
-    def backward(self, dE_dy):
+    # This function is the heart of the back-propagation algorithm. It does two
+    # things: (a) it computes the derivatives of its local weights and updates,
+    # and (b) it returns the derivatives of the output from the previous  layer.
+    # This is required in order to allow the previous layer to compute its own
+    # derivatives in (a).
+    #
+    # The implementation details - while important - are secondary to the fact
+    # that the role of this backward() function is to compute the derivatives of
+    # whatever we did in the forward() function. This symmetry gurantees that we
+    # can chain multiple layers together, even if the implementation of each one
+    # is completely different, and still achieve the correct results.
+    def backward(self, dy):
         x, y = self._last_inp, self._last_out
 
         # compute the derivatives - differentiate the reverse of the forward
         # pass. So for every mathematical operation in the forward pass, we need
         # the respective derivative in this backward pass. This is exactly like
-        # what we've done before.
-        dE_dw = np.array([d * x for d in dE_dy])
+        # what we've done before except that now we don't need to also compute
+        # the derivative of the total loss of each of our output neurons - it's
+        # given as the input to this layer from the next layer.
+        dw = np.array([d * x for d in dy])
 
         # before we update the weights, we'll compute our return value, which
         # will become the input to the previous layer - or how the total error
@@ -62,21 +74,41 @@ class Layer(object):
         # previous layer is the input to this current layer, this is equivalent
         # to the derivative w.r.t our input - so when we change the input - how
         # does the total error changes?
-
+        #
+        # This amount is equal to (a) how does our input affect our output,
+        # or dy/dx multiplied by (b) how does our output affects the total error
+        # which was given to us as input. We multiply these terms via the chain
+        # rule to chain these effects.
+        #
         # We start with the derivative of the output as a function of the input.
         # So when we change each input, how will the output be affected? Exactly
         # by the weight amounts of course, because: y = w1x1 + w2x2 + ...
         # So if we increase x1 by 1, y will increase by exacly w1.
-        dy_dx = self.W # how each input affects each of the outputs. MxN matrix
+        dy_dx = self.W # MxN matrix - rows are y, columns are x.
 
         # Now all that's left to know is how that output affects the error. We
-        # already know that! It's the input we received to this function.
-        dE_dx = np.dot(dE_dy, dy_dx)
+        # already know that! It's the input we received to this function (dy).
+        # All that's left to do is chain these two terms together. NOTE that
+        # this entire loop is can be re-written as: dx = np.dot(dy, dy_dx)
+        dx = np.zeros(len(x)) # our result - derivative of total loss w.r.t x
+        for i in xrange(len(y)):
+            # this y[i] affects the total loss by dy[i]. We know that. Now we
+            # need to compute how each input affects this y. This was computed
+            # before as dy_dx[i] - a 1xN matrix showing us the effect of each
+            # input on this y. So we just need to chain these terms together, to
+            # get the effect of each input on the total error. NOTE that as each
+            # output i is affected by all inputs, we need to sum up these
+            # effects per input to get the derivative of the total loss for each
+            # individual input
+            dx += dy[i] * dy_dx[i]
 
         # update
-        self.W -= ALPHA * dE_dw
+        self.W -= ALPHA * dw
 
-        return np.delete(dE_dx, -1) # remove the bias derivative
+        # we can remove the computed derivative of the final bias input because
+        # it was solely computed by this layer and it's not part of the output
+        # from the previous layer.
+        return np.delete(dx, -1)
 
 # build the two layers in the network
 l1 = Layer(Wxh)
@@ -113,7 +145,9 @@ d = y - T # start loss derivative at the top error layer. Same as before.
 d = l2.backward(d)
 _ = l1.backward(d)
 
-# print the updated weights
+# print the updated weights. NOTE that we can check our math by comparing the
+# weights from the back-prop algorithm with the results we received before with
+# pertubations. This is known as gradient checking.
 print "l1.W ="
 print l1.W # = (0.140640, 0.181281, 0.162808), (0.239475, 0.278951, 0.139499)
 print "l2.W ="
