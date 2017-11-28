13124202932f64e137635872c4cae00940fb2f5c
Roi Avinoam <avinoamr@gmail.com>
2017-11-27 21:06:46 -0800

    Initial implementation

--- 01_intro/13_features.py
+++ 01_intro/13_features.py
@@ -39,6 +39,13 @@
 # specific instance. In this example we'll combine the back-prop algorithm with
 # our full learning process.
 import numpy as np
+import csv
+import sys
+np.random.seed(1)
+
+ALPHA = .1
+EPOCHS = 100
+EPSILON = 0.0001
 
 # In this dataset we will attempt to learn the same data using different
 # configurations and then inspect the weights produced by the hidden layer to
@@ -67,5 +74,100 @@ class Layer(object):
         x, y = self._last
         dw = np.array([d * x for d in dy])
         dx = np.dot(dy, self.W)
-        self.W -= ALPHA * dw # update
-        return np.delete(dx, -1) # remove the bias derivative
+        # self.W -= ALPHA * dw # update
+        return np.delete(dx, -1), dw # remove the bias derivative
+
+# read the data from the CSV file
+data = [d for d in csv.DictReader(open("09_titanic.csv"))]
+N = 20
+BATCHSIZE = len(data) / 4
+
+vocabs = {
+    "Fare": { "cheap": 0, "low": 1, "medium": 2, "high": 3 },
+    "Embarked": { "S": 4, "C": 5, "Q": 6 },
+    "Age": { "kid": 7, "young": 8, "adult": 9, "old": 10 },
+    "Family": { "alone": 11, "small": 12, "medium": 13, "big": 14 },
+    "Pclass": { "1": 15, "2": 16, "3": 17 },
+    "Sex": { "male": 18, "female": 19 }
+}
+
+# encode the data into N input neurons
+def encode(d):
+    x = np.zeros(N)
+    for k, v in vocabs.items():
+        idx = v[d[k]]
+        x[idx] = 1.
+
+    return x
+
+l1 = Layer(np.random.random((2, N + 1)))
+l2 = Layer(np.random.random((2, 2 + 1)))
+
+# predict the output and loss for the given input and target
+def predict(x, t):
+    h = l1.forward(x)
+    y = l2.forward(h) # output from first layer is fed as input to the second
+
+    # now compute our error, same as before
+    e = (y - t) ** 2 /2
+    return y, e
+
+# learn the weights in all layers
+for i in xrange(EPOCHS):
+    np.random.shuffle(data)
+    l = 0
+    accuracy = 0
+
+    for j in xrange(0, len(data), BATCHSIZE):
+        minib = data[j:j+BATCHSIZE]
+        dw1 = 0
+        dw2 = 0
+
+        # print "l1.W=", l1.W
+        # print "l2.W=", l2.W
+        for d in minib:
+            x = encode(d) # encode the input features into multiple 1-of-key's
+            t = np.zeros(2)
+            t[int(d["Survived"])] = 1.
+            y, e = predict(x, t)
+            l += e
+
+            Ws = [l1.W, l2.W] # all of the weights in the network
+            dWs = [0, 0] # derivatives of all weights in both layers.
+            for j, w in enumerate(Ws): # iterate over all weight matrices in the network
+                dW = dWs[j] = np.zeros(w.shape)
+
+                # for every weight - re-run the entire network after applying a tiny change
+                # to that weight in order to measure how it affects the total loss.
+                for i in range(len(w)):
+                    for j in range(len(w[i])):
+                        w[i][j] += EPSILON # add a tiny epsilon amount to the weight
+                        _, e_ = predict(x, t) # re-run our network to predict the new error
+                        dW[i][j] = sum(e_ - e) / EPSILON
+                        w[i][j] -= EPSILON # revert our change.
+
+            # d, dWs[1] = l2.backward(y - t)
+            # _, dWs[0] = l1.backward(d)
+
+            dw1 += dWs[0]
+            dw2 += dWs[1]
+
+            idx = np.argmax(y)
+            if t[idx] == 1.:
+                accuracy += 1
+            # accuracy += 1 if round(np.clip(y, 0, 1)) == t else 0
+
+        # print dw1
+        dw1 /= len(minib)
+        dw2 /= len(minib)
+
+        # print dw1
+
+        # print "dw1=", dw1
+        # print "dw2=", dw2
+        l1.W += ALPHA * -dw1 # mini-batch update
+        l2.W += ALPHA * -dw2 # mini-batch update
+
+    print "%s: LOSS = %s; ACCURACY = %d of %d" % (i, l, accuracy, len(data))
+    print "l1.W=", l1.W
+    # print "l2.W=", l2.W
