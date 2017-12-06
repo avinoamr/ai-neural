b6acf8c6070f6517d0996b772953278d75932312
Roi Avinoam <avinoamr@gmail.com>
2017-12-05 22:04:46 -0800

    Remove the ReLU exercise as it's not used in future code

--- 01_intro/11_sigmoid.py
+++ 01_intro/11_sigmoid.py
@@ -31,6 +31,42 @@
 #
 # Much better! If anything, derivatives would stay unchanged, or get better.
 #
+# There are other types of activation functions worth mentioning, but will not
+# be covered further here - first is the hyperbolic function (tanh) which is
+# very similar to sigmoid:
+#
+#   y = tanh(z) = 2 * sigmoid(2z) - 1)
+#   dy/dz = tanh'(z) wrt z = 1 - y^2   # look it up on line to learn more
+#
+# It has a interesting property that it's mean is at zero (when z = 0, y = 0,
+# instead of y = .5 in sigmoid). We will not implement it here as it's a bit
+# redundant.
+#
+# Second is the Rectifier Linear Unit (ReLU):
+#
+#   y = max(0, z)
+#   dy/dz = { 1 when z > 0 ; 0 otherwise }
+#
+# It's basically just like having no activation function, except that negative
+# values are clamped at zero. A few things to note:
+#   1. It's still non-linear (below and above 0) and proven to be able to
+#       approximate any non-linear function when combined in multiple layers.
+#   2. It has the range of [0...inf] which means that it might be a bit
+#       inefficient when used with the squared difference loss function (it's
+#       more often used with softmax/cross-entropy - possibly discussed later).
+#   3. It generates sparse activations. Because negative y values are clamped at
+#       zero, many of the neurons (the irrelevant ones that do not contribute to
+#       the prediction of a given class) are not going to fire. This makes the
+#       model more elegant, simpler and easier to debug because we don't have a
+#       lot of negative numbers - we're only seeing the ones that actually
+#       affect the output. In a multi-layered network, it means that the input
+#       to the next layer is more likely to have a lot of zeros instead of a
+#       dense activations where all of the neruons have some value.
+#   4. Because the derivative is sometimes zero, it means that weights can get
+#       stuck at zero and stop responding to errors. This is called the dying
+#       ReLU problem and there are several variations, like Leaky ReLU that help
+#       prevent that (at a cost of sparsity)
+#
 # A NOTE about non-linearity: everything we've done until now is obviously
 # limited to linear problems. For example, our code cannot learn to approximate
 # the function f(x) = x^2 with any possible set of weights. One way around it
