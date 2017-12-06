d68f2d310743bb4d96d705fbe4abf0a2a7b20897
Roi Avinoam <avinoamr@gmail.com>
2017-12-06 14:30:33 -0800

    Layer class, still unused

--- 01_intro/30_titanic.py
+++ 01_intro/30_titanic.py
@@ -27,8 +27,7 @@ EPOCHS = 100
 # read the data from the CSV file and break the data into an input and output
 # sets, each is a list of (k,v) tuples
 data = [d for d in csv.DictReader(open("30_titanic.csv"))]
-
-N = 20
+T = [("Survived", d["Survived"]) for d in data]
 
 vocabs = [
     ("Fare", "cheap"), ("Fare", "low"), ("Fare", "medium"), ("Fare", "high"),
@@ -63,9 +62,38 @@ class OneHot(object):
     def decode(self, y):
         return self.alphabet[np.argmax(y)]
 
+class Layer(object):
+    W = None
+    _last = (None, None) # input, output
+
+    def __init__(self, n, m):
+        self.W = np.zeros((m, 1 + n)) # +1 for bias
+
+    def forward(self, x):
+        x = np.append(x, 1.) # add the fixed input for bias
+        y = np.dot(self.W, x) # derivate: x
+
+        self._last = x, y
+        return y
+
+    def backward(self, dy):
+        x, y = self._last
+
+        # derivatives of the loss with respect to the weights w
+        dw = np.array([d * x for d in dy])
+
+        # derivatives of the loss with respect to the input x
+        dy_dx = self.W # MxN matrix - rows are y, columns are x.
+        dx = np.dot(dy, dy_dx)
+
+        # update
+        return dw, np.delete(dx, -1) # remove the bias derivative from dx
+
 inp = OneHot(vocabs)
 out = OneHot([("Survived", "0"), ("Survived", "1")])
-w = np.zeros((2, 1 + N)) # +1 for bias
+# l = Layer(inp.N, out.N)
+
+w = np.zeros((out.N, 1 + inp.N)) # +1 for bias
 for i in xrange(EPOCHS):
     random.shuffle(data)
     l = 0
@@ -75,15 +103,17 @@ for i in xrange(EPOCHS):
         minib = data[j:j+BATCHSIZE]
         dw = 0
         for d in minib:
-            x = inp.encode(*d.items()) # encode the input features into multiple 1-of-key's
+            v = d.items()
+            target = ("Survived", d["Survived"])
+
+            x = inp.encode(*v) # encode the input features into multiple 1-of-key's
             x = np.insert(x, 0, 1.) # fixed bias
             y = np.dot(w, x) # compute the prediction
             res = out.decode(y) # a string, either "S" or "D"
-            target = ("Survived", d["Survived"])
             accuracy += 1 if res == target else 0 # simple as that!
 
             # loss and derivatives
-            t = out.encode(*d.items())
+            t = out.encode(target)
             l += (y - t) ** 2 / 2
             dy = y - t
             dw += np.array([dyi * x for dyi in dy]) # Mx(1 + N) derivatives
