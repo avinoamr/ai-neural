ad9440b2cb1b6d266b93edbb68989954b847ebbd
Roi Avinoam <avinoamr@gmail.com>
2017-12-07 02:05:26 -0800

    Pertubations with sigmoid

--- 01_intro/13_multi_layers.py
+++ 01_intro/13_multi_layers.py
@@ -81,7 +81,8 @@ class Layer(object):
     # forward pass - compute the predicted output for the given input
     def forward(self, x):
         x = np.append(x, 1.) # add the fixed input for bias
-        y = np.dot(self.W, x) # derivate: x
+        z = np.dot(self.W, x) # derivate: x
+        y = 1. / (1. + np.exp(-z)) # sigmoid non-linear activation
         return y
 
 # now lets create our two layers with the weights we've created before:
@@ -139,6 +140,6 @@ for W, dW in zip(Ws, dWs):
 
 # print the updated weights
 print "l1.W ="
-print l1.W # = (0.140640, 0.181281, 0.162808), (0.239475, 0.278951, 0.139499)
+print l1.W # = (0.149780, 0.199561, 0.345614), (0.249751, 0.299502, 0.345022)
 print "l2.W ="
-print l2.W # = (0.226794, 0.269912, 0.141162), (0.497235, 0.547125, 0.592662)
+print l2.W # = (0.358916, 0.408666, 0.530751), (0.511300, 0.561369, 0.619047)
